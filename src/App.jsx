068ebfff3a01251ee33dc40230b02ee3cd625c32
Alvaro Nieto <60838509+Anieto86@users.noreply.github.com>
2023-05-28 11:12:37 +0930

    theme default is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-// import { ThemeProvider } from '@mui/material';
+import { ThemeProvider } from '@mui/material';
 import './App.css';
-// import { createDefaultTheme } from './assets/theme/createDefaultTheme';
+import { createDefaultTheme } from './assets/theme/createDefaultTheme';
 import { AppRouterProvider } from './common/AppRouterProvider';
 
 function App() {
   return (
-    // <ThemeProvider theme={createDefaultTheme()}>
-    <AppRouterProvider />
-    // </ThemeProvider>
+    <ThemeProvider theme={createDefaultTheme()}>
+      <AppRouterProvider />
+    </ThemeProvider>
   );
 }
 
