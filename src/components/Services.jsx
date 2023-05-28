4818bc6161ffbb75b2516f546874fc3c3bf273b1
Alvaro Nieto <60838509+Anieto86@users.noreply.github.com>
2023-05-28 14:16:15 +0930

    adding accordion

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,20 +1,65 @@
-import { Button, Grid, Typography } from '@mui/material';
+import {
+  Button,
+  Grid,
+  Typography,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export const Services = () => {
   return (
     <Grid container spacing={2} columns={16}>
       <Grid item xs={16}>
-        <h2>OUR SERVICES</h2>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography variant="h5">OUR SERVICES</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Grid item>ICON </Grid>
+
+            <Grid item>
+              <Typography>
+                Our speciality is creating medical content for a wide variety of
+                packages. As key stakeholders of the project, our number one
+                priority is to work closely with you to create your science
+                story
+              </Typography>
+            </Grid>
+          </AccordionDetails>
+        </Accordion>
       </Grid>
-      <Grid item xs={6}>
-        image
-      </Grid>
-      <Grid item xs={8}>
-        <Typography>
-          Our speciality is creating medical content for a wide variety of
-          packages. As key stakeholders of the project, our number one priority
-          is to work closely with you to create your science story
-        </Typography>
+
+      <Grid item xs={16}>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography variant="h5">WHY US?</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Grid item xs={4}>
+              ICON{' '}
+            </Grid>
+
+            <Grid item xs={8}>
+              <Typography>
+                As scientists turned artists, we bring a unique blend of
+                expertise and years of experience serving academia, biotech, and
+                the health industry. Collaborating closely with you, we apply
+                critical thinking and creativity to tackle your communication
+                challenges.
+              </Typography>
+            </Grid>
+          </AccordionDetails>
+        </Accordion>
       </Grid>
 
       <Grid item xs={16}>
@@ -29,20 +74,7 @@ export const Services = () => {
           Contact Us
         </Button>
       </Grid>
-      <Grid item xs={16}>
-        <h2>WHY US?</h2>
-      </Grid>
-      <Grid item xs={6}>
-        image
-      </Grid>
-      <Grid item xs={8}>
-        <Typography>
-          As scientists turned artists, we bring a unique blend of expertise and
-          years of experience serving academia, biotech, and the health
-          industry. Collaborating closely with you, we apply critical thinking
-          and creativity to tackle your communication challenges.
-        </Typography>
-      </Grid>
+
       <Grid item xs={16}>
         <h2>WHO WE’VE WORK WITH?</h2>
       </Grid>
