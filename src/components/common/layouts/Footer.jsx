ed72d97d4c7961203aaf60ff18f82e1d236578f5
Anieto86 <alvaro16nieto@gmail.com>
2024-05-31 15:45:19 +0930

    new footer

--- src/components/common/layouts/Footer.jsx
+++ src/components/common/layouts/Footer.jsx
@@ -1,106 +1,117 @@
 import { Box, Grid, IconButton, Typography } from '@mui/material';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import XIcon from '@mui/icons-material/X';
 import { Link } from 'react-router-dom';
+import logo from '../../../assets/img/ColourLogo.png';
 
 export const Footer = () => {
   return (
-    <Box
-      sx={{
-        bottom: 0,
-        left: 0,
-        width: '100%',
-        zIndex: 999,
-        backgroundColor: '#F6F7F9',
-        padding: 2,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        mt: 20,
-      }}
-    >
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          mt: 5,
-        }}
+    <Grid container padding={6}>
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        xs={4}
+        sx={{ border: '1px solid green' }}
       >
-        <Link to={`https://www.instagram.com/chemdye_si/?hl=es`}>
-          <IconButton
-            sx={{
-              color: 'black',
-              fontSize: 30,
-            }}
-          >
-            <InstagramIcon sx={{ fontSize: '1.5em' }} />
-          </IconButton>
-        </Link>
-        <Link to={`https://www.linkedin.com/in/mariana-oksdath-mansilla`}>
-          <IconButton
-            sx={{
-              color: 'black',
-              mx: 2,
-              fontSize: 30,
-            }}
-          >
-            <LinkedInIcon sx={{ fontSize: '1.5em' }} />
-          </IconButton>
-        </Link>
-
-        <Link
-          to={`https://twitter.com/i/flow/login?redirect_after_login=%2FMari_SciArt`}
-        >
-          <IconButton
-            sx={{
-              color: 'black',
-              fontSize: 30,
-            }}
-          >
-            <TwitterIcon sx={{ fontSize: '1.5em' }} />
-          </IconButton>
-        </Link>
-      </Box>
-
-      <Box
-        sx={{
-          width: '100%',
-          height: 2,
-          backgroundColor: '#ff6f80',
-          marginTop: 2,
-        }}
-      ></Box>
-
-      <Grid container spacing={2} columns={16} sx={{ p: 2 }}>
-        <Grid item xs={10}>
-          <Typography align="left" sx={{ fontSize: 15 }}>
-            Terms of use / Cookie Policy / Privacy Policy
-          </Typography>
-          <Grid item sx={{ my: 3 }}>
-            <Typography align="left" sx={{ fontSize: 15 }}>
-              ChemDye Scientific Illustrations
-            </Typography>
-            <Typography align="left" sx={{ fontSize: 15 }}>
-              ABN 45 841 581 765
-            </Typography>
-            <Typography align="left" sx={{ fontSize: 15 }}>
-              Adelaide - Australia
-            </Typography>
-          </Grid>
+        <Grid item>
+          <img src={logo} alt="Logo footer" />
+        </Grid>{' '}
+        <Grid item>
+          <Typography>ChemDye</Typography>
         </Grid>
-        <Grid item xs={6}>
-          <Typography
-            align="right"
-            sx={{ fontSize: 15, p: 2, lineHeight: '1.2' }}
+      </Grid>
+
+      <Grid
+        container
+        xs={8}
+        direction="row"
+        justifyContent="flex-end"
+        alignItems="center"
+        sx={{ border: '1px solid green' }}
+      >
+        <Grid item>
+          <Link to={`https://www.instagram.com/chemdye_si/?hl=es`}>
+            <IconButton
+              sx={{
+                color: '#DF367B',
+                background: 'rgb(226, 139, 174)',
+              }}
+            >
+              <InstagramIcon />
+            </IconButton>
+          </Link>
+          <Link to={`https://www.linkedin.com/in/mariana-oksdath-mansilla`}>
+            <IconButton
+              sx={{
+                color: '#DF367B',
+                background: 'rgb(226, 139, 174)',
+              }}
+            >
+              <LinkedInIcon />
+            </IconButton>
+          </Link>
+
+          <Link
+            to={`https://twitter.com/i/flow/login?redirect_after_login=%2FMari_SciArt`}
           >
-            Unauthorized Reproduction Prohibited: Any replication, utilization,
-            or incorporation of materials including images, diagrams, sounds, or
-            text from this website in electronic or printed publications without
-            prior written consent is strictly prohibited.
+            <IconButton
+              sx={{
+                color: '#DF367B',
+                background: 'rgb(226, 139, 174)',
+              }}
+            >
+              <XIcon />
+            </IconButton>
+          </Link>
+        </Grid>
+      </Grid>
+
+      <Grid container sx={{ border: '1px solid blue' }} s>
+        <Grid item xs={5} mb={3}>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam at
+            perspiciatis a mollitia excepturi qui iusto odio, maiores magnam,
+            quam quaerat accusantium, atque aspernatur perferendis. Optio, id!
+            Sit, modi quam!
           </Typography>
         </Grid>
       </Grid>
-    </Box>
+
+      <Grid
+        container
+        justifyContent="space-between"
+        alignItems="center"
+        columns
+        sx={{ border: '1px solid blue' }}
+      >
+        <Grid item>
+          <Typography variant="h5">About ChemDye studio</Typography>
+          <Typography>Meet the team</Typography>
+          <Typography>Explore our work</Typography>
+          <Typography>What we do</Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="h5">Site information & Policies</Typography>
+          <Typography>About this website</Typography>
+          <Typography>Privacy Policy</Typography>
+          <Typography>Cookies</Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="h5">Get in Touch</Typography>
+          <Typography>si.chemdye@gmail.com</Typography>
+          <Typography>xxxxxxxxxxxxx</Typography>
+        </Grid>
+      </Grid>
+      <Box
+        sx={{ width: '100%', height: 1.5, backgroundColor: '#000000', my: 2 }}
+      ></Box>
+      <Grid container justifyContent="flex-end">
+        <Grid item>
+          <Typography>c 2024, All Rights Reserved</Typography>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
