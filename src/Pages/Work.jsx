794dcf1e82c0e3117ed11d5294f0ce3d5532c1d5
Anieto86 <alvaro16nieto@gmail.com>
2024-06-29 17:48:06 +0930

    remove inecesary grid

--- src/Pages/Work.jsx
+++ src/Pages/Work.jsx
@@ -12,38 +12,37 @@ export function Work() {
         title="Our journey into creativity"
         subTitle="ChemDye collaborates with scientists across diverse disciplines, driving innovation through creative solutions."
       />
-      <Grid item xs={10}>
-        <Masonry columns={{ xs: 1, md: 3, sm: 2 }}>
-          {urlImgOrganize.map((portfolio, index) => (
-            <Grid
-              key={index}
-              sx={{
-                transition: 'transform 0.2s ease-in-out',
-                '&:hover': {
-                  transform: 'scale(1.4)',
-                },
-              }}
-            >
-              <Box style={{ overflow: 'hidden', borderRadius: '4px' }}>
-                <img
-                  src={`${portfolio.url}?w=162&auto=format`}
-                  srcSet={`${portfolio.url}?w=162&auto=format&dpr=2 2x`}
-                  alt={portfolio.description}
-                  loading="lazy"
-                  style={{
-                    border: '0.7px #616161 solid',
-                    borderRadius: 10,
-                    width: '99%',
-                    height: 'auto',
-                    transition: 'transform 0.2s ease-in-out',
-                    backgroundColor: '#ffffff',
-                  }}
-                />
-              </Box>
-            </Grid>
-          ))}
-        </Masonry>
-      </Grid>
+
+      <Masonry columns={{ xs: 1, md: 3, sm: 2 }}>
+        {urlImgOrganize.map((portfolio, index) => (
+          <Grid
+            key={index}
+            sx={{
+              transition: 'transform 0.2s ease-in-out',
+              '&:hover': {
+                transform: 'scale(1.4)',
+              },
+            }}
+          >
+            <Box style={{ overflow: 'hidden', borderRadius: '4px' }}>
+              <img
+                src={`${portfolio.url}?w=162&auto=format`}
+                srcSet={`${portfolio.url}?w=162&auto=format&dpr=2 2x`}
+                alt={portfolio.description}
+                loading="lazy"
+                style={{
+                  border: '0.7px #616161 solid',
+                  borderRadius: 10,
+                  width: '99%',
+                  height: 'auto',
+                  transition: 'transform 0.2s ease-in-out',
+                  backgroundColor: '#ffffff',
+                }}
+              />
+            </Box>
+          </Grid>
+        ))}
+      </Masonry>
     </>
   );
 }
