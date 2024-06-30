38e871f7cf00540854b92449e10c4b9749fc9920
Anieto86 <alvaro16nieto@gmail.com>
2024-06-30 17:24:13 +0930

    fix title responsivity

--- src/components/common/TitleHeader.jsx
+++ src/components/common/TitleHeader.jsx
@@ -1,27 +1,29 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, useMediaQuery } from '@mui/material';
 import { Link, useLocation } from 'react-router-dom';
 import { ContactBtn } from './ContactBtn';
+import { useTheme } from '@emotion/react';
 
 // eslint-disable-next-line react/prop-types
 export const TitleHeader = ({ title, subTitle }) => {
   let location = useLocation();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   return (
-    <>
-      <Typography variant="h1" sx={{ mt: 30 }}>
+    <Grid>
+      <Typography variant={isMobile ? 'h2' : 'h1'} sx={{ mt: 30 }}>
         {title}
       </Typography>
-      <Typography variant="h4" sx={{ mt: 3 }}>
+      <Typography variant={isMobile ? 'h5' : 'h4'} sx={{ mt: 3 }}>
         {subTitle}
       </Typography>
 
       {location.pathname !== '/contact' && (
         <Grid item xs={9.5} sx={{ my: 10 }}>
-          {' '}
           <Link to={`/contact`}>
             <ContactBtn title="Connect with us" />
           </Link>
         </Grid>
       )}
-    </>
+    </Grid>
   );
 };
